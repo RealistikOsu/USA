f853b4d527a44de5389226eb7c65a79699a56ffc
James Wilson <tsunyoku@gmail.com>
2024-07-24 23:34:12 +0100

    fix direct beatmapset format

--- src/adapters/osu_direct.ts
+++ src/adapters/osu_direct.ts
@@ -2,7 +2,10 @@ import { CheesegullBeatmap, CheesegullBeatmapset } from "./cheesegull";
 
 export function osuDirectBeatmapsetFromCheesegullBeatmapset(beatmapset: CheesegullBeatmapset): string {
     const difficultySortedBeatmaps = beatmapset.beatmaps.sort((a, b) => a.difficultyRating - b.difficultyRating);
-    return difficultySortedBeatmaps.map(beatmap => osuDirectBeatmapFromCheesegullBeatmap(beatmap)).join(",");
+    const formattedBeatmaps = difficultySortedBeatmaps.map(beatmap => osuDirectBeatmapFromCheesegullBeatmap(beatmap)).join(",");
+
+    // TODO: replace placeholder values
+    return `${beatmapset.id}.osz|${beatmapset.artist}|${beatmapset.title}|${beatmapset.creator}|${beatmapset.rankedStatus}|10.0|${beatmapset.lastUpdate}|${beatmapset.id}|0|${beatmapset.hasVideo ? 1 : 0}|0|0|0|${formattedBeatmaps}`;
 }
 
 export function osuDirectBeatmapFromCheesegullBeatmap(beatmap: CheesegullBeatmap): string {
