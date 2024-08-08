be437783a900df5dfa55960bb397f6eb1f470576
James Wilson <tsunyoku@gmail.com>
2024-08-08 17:57:31 +0100

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,19 @@ import fastify from "fastify";
 
 import { registerContext } from "./context";
 import { createRoutes } from "./routes";
+import { Logger } from "./logger";
+import { HttpStatusCode } from "axios";
 
 export const createApp = async () => {
+    const logger: Logger = new Logger();
     const server = fastify();
+
+    server.setErrorHandler((error, request, reply) => {
+        logger.error("Received error", { error });
+
+        reply.status(HttpStatusCode.InternalServerError);
+        reply.send();
+    })
 
     server.register(fastifyMultipart);
 
